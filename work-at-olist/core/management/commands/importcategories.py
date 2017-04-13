c61080f4f87330f08ebb0fbc995e73758e26db2c
Manoel Vilela <manoel_vilela@engineer.com>
2017-04-13 09:13:37 -0300

    Implement the importcategories Django Management Command

--- work-at-olist/core/management/commands/importcategories.py
+++ work-at-olist/core/management/commands/importcategories.py
@@ -1,22 +1,71 @@
+import csv
+
 from django.core.management import BaseCommand
 
+from core.models import Category, Channel
 
-# return a tree-like structure representing
-# the categories as children and parents
+
 def read_csv(fpath):
-    pass
+    """Return a generator of lines from a csv file"""
+    with open(fpath) as csv_file:
+        csv_content = csv.reader(csv_file, delimiter=',')
+        next(csv_content)  # drop header
+        for line in csv_content:
+            yield line[0]  # use generator to be memory-friendly
+
+
+def overwrite_channel(channel_name):
+    """
+    Create a new channel, save and return it
+    if already exists, before return drop all its categories related
+    """
+    # try delete before
+    try:
+        Channel.objects.get(name=channel_name).delete()
+    except:
+        pass
+    finally:  # create a new channel anyways
+        channel = Channel.objects.create(name=channel_name)
+        channel.save()
+        return channel
+
+
+def get_last_parent(categories, channel):
+    """
+    Fetch iteratively the category path list until
+    get the last parent
+    """
+    if len(categories) == 1:
+        return None
+
+    parent = None
+    for name in categories[:-1]:
+        parent = Category.objects.get(name=name,
+                                      parent=parent,
+                                      channel=channel)
+    return parent
 
 
 class Command(BaseCommand):
+
     help = 'Import categories to a channel given a csv file with categories.'
 
     def add_arguments(self, parser):
-        parser.add_argument('channel')
-        parser.add_argument('categories')
+        """Add the channel & categories as arguments"""
+        parser.add_argument('channel_name')
+        parser.add_argument('csv_categories')
 
     def handle(self, *args, **options):
-        channel = options['channel']
-        csv = options['categories']
-        print("Channel: {}".format(channel))
-        print("Csv: {}".format(csv))
-        print('To be implemented.')
+        channel_name = options['channel_name']
+        channel = overwrite_channel(channel_name)
+        csv_path = options['csv_categories']
+        for category_path in read_csv(csv_path):
+            parent = None
+            categories = [x.strip() for x in category_path.split('/')]
+            parent = get_last_parent(categories, channel)
+            name = categories[-1]
+            (cat, created) = Category.objects.get_or_create(name=name,
+                                                            parent=parent,
+                                                            channel=channel)
+            if created:
+                cat.save()
