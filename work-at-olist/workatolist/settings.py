a00c341c0d6390fa11a70f18b6fc577915a2dfda
Manoel Vilela <manoel_vilela@engineer.com>
2017-04-13 16:10:33 -0300

    Add settings to create another database during unit tests
    This avoid modify data from the default database local. A new
    database sqlite3 will be created called auto-test-db.sqlite3 during
    the tests and will be destroyed on finish.

--- work-at-olist/workatolist/settings.py
+++ work-at-olist/workatolist/settings.py
@@ -68,6 +68,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'TEST': {
+            'NAME': 'auto-test-db.sqlite3'
+        }
     }
 }
 
