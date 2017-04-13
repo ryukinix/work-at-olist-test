40e4083ec38f1fc9f144ff15e3df997835521482
Manoel Vilela <manoel_vilela@engineer.com>
2017-04-13 10:39:49 -0300

    Add the core.urls namespace at /api/ on workatolist.urls
    The overall api will be:
    1. /api/channels/
    2. /api/channels/<channel>/
    3. /api/channels/<channel>/<category>/
    
    Explanation:
    1. list the name of channels and its identifiers
    2. list the category and subcategories for that channel
    3. Return a single category with their parents and subcategories

--- work-at-olist/workatolist/urls.py
+++ work-at-olist/workatolist/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^api/', include('core.urls', namespace='core'))
 ]
