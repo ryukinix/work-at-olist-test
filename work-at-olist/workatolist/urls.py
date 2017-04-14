b773545be843fdab70e106f7b35a6035f4d02974
Manoel Vilela <manoel_vilela@engineer.com>
2017-04-14 15:23:05 -0300

    Add shortcut.redirect from root of the to /api/channels/

--- work-at-olist/workatolist/urls.py
+++ work-at-olist/workatolist/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-
+from django.shortcuts import redirect
 
 urlpatterns = [
+    url('^$', lambda request: redirect('core:channel-list')),
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('core.urls', namespace='core'))
 ]
