03e65d6986708532d022b49cdabba6a6e5b05546
Manoel Vilela <manoel_vilela@engineer.com>
2017-04-14 15:03:51 -0300

    Remove BASE_DIR/static from settings
    This throw a exception because doesn't exists. I don't write
    any static file, so this folder is unecessary.

--- work-at-olist/workatolist/settings.py
+++ work-at-olist/workatolist/settings.py
@@ -141,11 +141,6 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
